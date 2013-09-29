5497b97840d21dbf5bafaf44e8b379671586f88e
balam palma <balamcantzin at gmail.com>
2013-09-29 14:01:28 -0600

    Modify admin functions include

--- lifeband-plugin.php
+++ lifeband-plugin.php
@@ -37,5 +37,27 @@ function lifeband_plugin_init(){
 function run_sub_process(){
     //more work
 }
-include_once ('init-admin.php');
+
+/* INITIALIZE THE ADMIN FUNCTIONS */
+
+//1
+function lifeband_plugin_menu() {
+    add_options_page('Super Plugin Options', 'Super Plugin', 'manage_options', 'lifeband-plugin-menu', 'lifeband_plugin_options');
+}
+
+function register_mysettings() {
+//register our settings
+    register_setting('lifeband-settings-group', 'new_option_name');
+    register_setting('lifeband-settings-group', 'some_other_option');
+    register_setting('lifeband-settings-group', 'option_etc');
+}
+
+//2
+add_action('admin_menu', 'lifeband_plugin_menu');
+
+//3
+function lifeband_plugin_options() {
+    include('admin/lifeband-plugin-admin.php');
+}
+
 ?>
