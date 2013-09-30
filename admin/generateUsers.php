6721a9c5bc4ac08f71625787c1a367f46b52f14c
el-aro <aro.chomsky@gmail.com>
2013-09-29 18:00:03 -0600

    POO
    cambios para que se pueda mandar llamar explicitamente el generar
    usuarios.

--- admin/generateUsers.php
+++ admin/generateUsers.php
@@ -12,45 +12,49 @@
 	    }
 	    return $randomString;
 }
-	class generateUsers($cant=50){
-		$i = 0;
-		$nombreUsuario = '';
-		$pass = '';
-		$lastId = 0;
-		While ($i <= $cant) DO{
-			$lastId = $wpdb->get_var("SELECT id INTO lastId from ". $wpdb->prefix."users order by id desc limit 1");
-			$lastId = $lastId + 1;
-			$passusr = wp_generate_password(8);
-			$pass = wp_hash_password($passusr);
-			$nombreUsuario = substr(sha1(lastId),1,4).strrev(lastId);
-			$correo = $nombreUsuario.'@lifeband.com';
-
+	class generateUsers{
+			global $i = 0;
+			global $nombreUsuario = '';
+			global $pass = '';
+			global $lastId = 0;
+		function canti($cant=50){
 			
-			$usersTb = array(
-		        'user_nicename' => $nombreUsuario,
-		        'email' => $correo,
-		        'password' => $pass,
-		        'user_registered' => gmdate('Y-m-d H:i:s'),
-		        'user_status' => 0
-		    );
-		    $usermetaTb = array(
-				'nickname'=> $nombreUsuario,
-				'rich_editing'=>'true',
-				'comment_shortcuts'=> 'false',
-				'admin_color'=> 'fresh',
-				'use_ssl'=> '0',
-				'show_admin_bar_front'=> 'true'),
-				'wp_capabilities'=> 'a:1:{s:10:"subscriber";b:1;}',
-				'wp_user_level'=> '0',
-				'dismissed_wp_pointers'=> 'wp330_toolbar,wp330_saving_widgets,wp340_choose_image_from_library,wp340_customize_current_theme_link,wp350_media,wp360_revisions,wp360_locks';
-			);
-			$userPassTb = array('id' => $lastId,
-			'pass' => $passusr );
+			While ($i <= $cant) DO{
+				$lastId = $wpdb->get_var("SELECT id INTO lastId from ". $wpdb->prefix."users order by id desc limit 1");
+				$lastId = $lastId + 1;
+				$passusr = wp_generate_password(8);
+				$pass = wp_hash_password($passusr);
+				$nombreUsuario = substr(sha1(lastId),1,4).strrev(lastId);
+				$correo = $nombreUsuario.'@lifeband.com';
 
-			$wpdb->insert($wpdb->prefix.'users', $usersTb);
-			$wpdb->insert($wpdb->prefix.'usermeta', $usermetaTb);
-			$wpdb->insert($wpdb->prefix.'pass_qr', $userPassTb);
-			$i++;
+				
+				$usersTb = array(
+			        'user_nicename' => $nombreUsuario,
+			        'email' => $correo,
+			        'password' => $pass,
+			        'user_registered' => gmdate('Y-m-d H:i:s'),
+			        'user_status' => 0
+			    );
+			    $usermetaTb = array(
+					'nickname'=> $nombreUsuario,
+					'rich_editing'=>'true',
+					'comment_shortcuts'=> 'false',
+					'admin_color'=> 'fresh',
+					'use_ssl'=> '0',
+					'show_admin_bar_front'=> 'true'),
+					'wp_capabilities'=> 'a:1:{s:10:"subscriber";b:1;}',
+					'wp_user_level'=> '0',
+					'dismissed_wp_pointers'=> 'wp330_toolbar,wp330_saving_widgets,wp340_choose_image_from_library,wp340_customize_current_theme_link,wp350_media,wp360_revisions,wp360_locks';
+				);
+				$userPassTb = array('id' => $lastId,
+				'pass' => $passusr );
+
+				$wpdb->insert($wpdb->prefix.'users', $usersTb);
+				$wpdb->insert($wpdb->prefix.'usermeta', $usermetaTb);
+				$wpdb->insert($wpdb->prefix.'pass_qr', $userPassTb);
+				$i++;
+			}
+
 	}
 
 	}
