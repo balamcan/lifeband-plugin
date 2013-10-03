e8848eecba231ed6feef55bfaa367a38cbea0fa0
balam palma <balamcantzin at gmail.com>
2013-10-03 16:45:34 -0600

    adding changes of the install plugin

--- lifeband-plugin.php
+++ lifeband-plugin.php
@@ -1,11 +1,9 @@
 <?php
 /*
-Plugin Name: Life Band plugin
-Plugin URI: http://none.com
+Plugin Name: Life Band Plugin
 Description: A brief description of the Plugin.
-Version: 0.4
+Version: 1.0
 Author: Balam Palma
-Author URI: http://none.com
 License: GPL2
 */
 
@@ -28,7 +26,79 @@ function lifeband_plugin_install(){
        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
        dbDelta($sql);
        add_option( "jal_db_version", $jal_db_version );
-       
+       /*creating calatogos*/
+    $wpdb->query("DROP TABLE IF EXISTS `wp_cat_tipo_diabetes`;
+            CREATE TABLE IF NOT EXISTS `wp_cat_tipo_diabetes` (
+           `id` int(11) NOT NULL AUTO_INCREMENT,
+           `nombre` varchar(20) NOT NULL,
+           PRIMARY KEY (`id`)
+                ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+                INSERT INTO `wp_cat_tipo_diabetes` (`id`, `nombre`) VALUES
+           (1, 'Ninguna'),
+           (2, 'Tipo 1'),
+           (3, 'Tipo 3'),
+           (4, 'Gestacional');");
+    $wpdb->query("DROP TABLE IF EXISTS `wp_cat_tipo_sangre`;
+            CREATE TABLE IF NOT EXISTS `wp_cat_tipo_sangre` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `nombre` varchar(3) NOT NULL,
+            PRIMARY KEY (`id`)
+          ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+          INSERT INTO `wp_cat_tipo_sangre` (`id`, `nombre`) VALUES
+          (1, 'O-'),
+          (2, 'O+'),
+          (3, 'A-'),
+          (4, 'A+'),
+          (5, 'B-'),
+          (6, 'B+'),
+          (7, 'AB-'),
+          (8, 'AB+');");
+    //the others tables
+            $wpdb->query("CREATE TABLE IF NOT EXISTS `wp_datos_medicos` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `wp_users_id` int(11) NOT NULL,
+          `wp_cat_tipo_sangre_id` int(11) NOT NULL,
+          `wp_cat_tipo_diabetes_id` int(11) NOT NULL,
+          `presion_arterial_diastolica` int(11) DEFAULT NULL,
+          `presion_arterial_sistolica` int(11) DEFAULT NULL,
+          `donador_organos` tinyint(1) DEFAULT NULL,
+          `alergias` text,
+          `medicamentos` text,
+          `enfermedades` text,
+          `cirugias` text,
+          `otras_consideraciones` text,
+          `d_auditiva` text,
+          `d_mental` text,
+          `d_motora` text,
+          `d_visual` text,
+          `marcapasos` text,
+          `lentes_contacto` text,
+          `p_dentales` text,
+          `p_oculares` text,
+          `med_natural` text,
+          PRIMARY KEY (`id`),
+          KEY `fk_wp_dat_med_wp_users1_idx` (`wp_users_id`),
+          KEY `fk_wp_dat_med_wp_cat_tipo_san_idx` (`wp_cat_tipo_sangre_id`),
+          KEY `fk_wp_datos_medicos_wp_cat_tipo_diab_idx` (`wp_cat_tipo_diabetes_id`)
+          ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;");
+            $wpdb->query("CREATE TABLE IF NOT EXISTS `wp_datos_basicos` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `wp_users_id` int(11) NOT NULL,
+          `nombre` varchar(120) NOT NULL,
+          `ap_paterno` varchar(55) DEFAULT NULL,
+          `ap_materno` varchar(55) DEFAULT NULL,
+          `encargado_emergencia` varchar(70) DEFAULT NULL,
+          `tel_emergencia` varchar(45) DEFAULT NULL,
+          `correo_emergencia` varchar(70) DEFAULT NULL,
+          `nom_medico` varchar(70) DEFAULT NULL,
+          `tel_medico` varchar(45) DEFAULT NULL,
+          `fecha_nac` date DEFAULT NULL,
+          `peso` decimal(2,0) DEFAULT NULL,
+          `estatura` decimal(4,2) DEFAULT NULL,
+          `sexo` varchar(1) DEFAULT NULL,
+          PRIMARY KEY (`id`),
+          KEY `fk_wp_datos_basicos_wp_users1_idx` (`wp_users_id`)
+        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;");
     /*Create additional tables for the custom database*/
 }
 register_activation_hook(__FILE__,'lifeband_plugin_install');
