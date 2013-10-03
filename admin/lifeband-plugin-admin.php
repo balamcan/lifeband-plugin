bc5574057806aac1b628b95f6dce52e3717a5c00
balam palma <balamcantzin at gmail.com>
2013-10-03 16:33:18 -0600

    many changes of the templates and plugin admin lifeband

--- admin/lifeband-plugin-admin.php
+++ admin/lifeband-plugin-admin.php
@@ -1,7 +1,9 @@
 <?php
 require_once('generateUsers.php');
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
+if(isset($_POST['submit'])) { 
+	generateUsers::canti();
+} 
 ?>
 
 
@@ -11,13 +13,16 @@ require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
 <h3>Life Band Plugin Options</h3>
 
-</form>
 <form method="post" action="lifeband-plugin-admin.php">
-<input type="button" name="submit" value="click me">
+<input type="button" name="submit" value="Generar 50 usuarios">
 </form>
 </div>
+<table>
+<tr><th>Nombre usuario</th><th>Contrase&ntilde;a</th></tr>
 <?php 
-if(isset($_POST['submit'])) { 
-generateUsers::canti();
-} 
+$usuarios=$wpdb->get_results('select u.user_login as username, p.pass from wp_users as u LEFT JOIN wp_pass_qr as p on u.id = p.id_user', OBJECT);
+foreach ($usuarios as $u) {
+echo'<tr><td>'.$u->username.'</td><td>'.$u->pass.'</td></tr>';
+}
 ?>
+</table>
