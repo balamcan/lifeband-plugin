69f5bab0848f5127478ab3b2f0a5c5f6b18a83e8
el-aro <aro.chomsky@gmail.com>
2013-10-03 22:28:44 -0600

    admin
    aun no jala el boton

--- admin/lifeband-plugin-admin.php
+++ admin/lifeband-plugin-admin.php
@@ -3,10 +3,10 @@ require_once('generateUsers.php');
 include_once(ABSPATH . '/wp-includes/wp-db.php');
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 $userFactory = new generateUsers();
-//if(isset($_POST['submit'])) { 
+if(isset($_POST['submit'])) { 
     $userFactory->canti();
     echo "50 Usuarios Generados";
-//} 
+} 
 ?>
 
 
@@ -16,7 +16,7 @@ $userFactory = new generateUsers();
 
 <h3>Life Band Plugin Options</h3>
 
-<form method="post" action="#">
+<form method="post" action="/wordpress/wp-content/plugins/lifeband-plugin/admin/lifeband-plugin-admin.php">
 <input type="button" name="submit" value="Generar 50 usuarios">
 </form>
 </div>
