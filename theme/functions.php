6062454d221e24ffa6a8fa67d50314a3bbadd8cc
balam palma <balamcantzin at gmail.com>
2013-10-07 21:24:45 -0600

    Last commit... i think...3.0

--- theme/functions.php
+++ theme/functions.php
@@ -563,5 +563,40 @@ function redirect_user_on_role() {
 //        return $redirect_to;
 //    }
 }
-
-add_action('admin_init', 'redirect_user_on_role');
+/*
+Plugin Name: Allow Email Login
+Plugin URI: http://en.bainternet.info
+Description: Username Allow Email LogIn
+Version: 1.0
+Author: Bainternet
+Author URI: http://en.bainternet.info
+*/
+ 
+add_filter('authenticate', 'bainternet_allow_email_login', 20, 3);
+/**
+ * bainternet_allow_email_login filter to the authenticate filter hook, to fetch a username based on entered email
+ * @param  obj $user
+ * @param  string $username [description]
+ * @param  string $password [description]
+ * @return boolean
+ */
+function bainternet_allow_email_login( $user, $username, $password ) {
+    if ( is_email( $username ) ) {
+        $user = get_user_by_email( $username );
+        if ( $user ) $username = $user->user_login;
+    }
+    return wp_authenticate_username_password(null, $username, $password );
+}
+ 
+add_filter( 'gettext', 'addEmailToLogin', 20, 3 );
+/**
+ * addEmailToLogin function to add email address to the username label
+ * @param string $translated_text   translated text
+ * @param string $text              original text
+ * @param string $domain            text domain
+ */
+function addEmailToLogin( $translated_text, $text, $domain ) {
+    if ( "Username" == $translated_text )
+        $translated_text .= __( ' Or Email');
+    return $translated_text;
+}
